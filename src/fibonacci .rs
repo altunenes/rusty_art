58a5a2c619a48da426ca73be57ef4ad6fd8f7070
altunenes <enesaltun2@gmail.com>
2023-03-06 04:09:27 +0300

    fibonacci spirals

---
+++ src/fibonacci .rs
@@ -0,0 +1,47 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {
+    points: Vec<Point2>,
+    angle: f32,
+    scale: f32,
+}
+
+fn model(_app: &App) -> Model {
+    Model {
+        points: Vec::new(),
+        angle: 0.0,
+        scale: 1.0,
+    }
+}
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    let golden_angle = (3.0 - 5.0_f32.sqrt()) * std::f32::consts::PI;
+    let r = model.points.len() as f32 * model.scale;
+    let x = r * model.angle.cos();
+    let y = r * model.angle.sin();
+    let pos = pt2(x, y);
+    model.points.push(pos);
+    model.angle += golden_angle;
+}
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(WHITE);
+    for (i, &pos) in model.points.iter().enumerate() {
+        let hue = i as f32 / model.points.len() as f32;
+        let color = hsla(hue, 1.0, 0.5, 1.0);
+        let radius = (i as f32).sqrt() * 2.0;
+        draw.ellipse()
+            .xy(pos)
+            .radius(radius)
+            .color(color)
+            .stroke_weight(1.0)
+            .stroke(WHITE);
+    }
+    draw.to_frame(app, &frame).unwrap();
+}
