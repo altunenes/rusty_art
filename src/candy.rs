94f7036e89b5d8e1a23cf5c5ab879d211e84f33c
altunenes <enesaltun2@gmail.com>
2023-04-07 04:34:00 +0300

    typo

--- src/candy.rs
+++ src/candy.rs
@@ -54,13 +54,6 @@ fn view(app: &App, model: &Model, frame: Frame) {
             .stroke_weight(1.0)
             .stroke(color);
     }
-    if app.elapsed_frames() % 1 == 0 {
-        let file_path = app
-            .project_path()
-            .expect("failed to locate project directory")
-            .join(format!("{:1}.png", app.elapsed_frames()));
-        app.main_window().capture_frame(file_path);
-    } 
     draw.to_frame(app, &frame).unwrap();
     if app.keys.down.contains(&Key::Space) {
         let file_path = app
