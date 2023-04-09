009fb1fc1b91c359424c60c68573d3997d55050d
altunenes <enesaltun2@gmail.com>
2023-04-09 21:17:44 +0300

    gui

--- src/pixely.rs
+++ src/pixely.rs
@@ -1,42 +1,77 @@
 use nannou::prelude::*;
-
+use nannou_egui::{self, egui, Egui};
 fn main() {
-    nannou::app(model)
-        .update(update)
-        .simple_window(view)
-        .run();
+    nannou::app(model).update(update).run();
 }
-
 struct Model {
     lines: Vec<(Point2, Point2)>,
-    angle: f32,
-    speed: f32,
-    line_length: f32,
+    speed : f32,
+    egui: Egui,
+    settings: Settings,
     
 }
 
-fn model(_app: &App) -> Model {
+struct Settings {
+    angle: f32,
+    line_length: f32,
+     r: f32,
+    line_count: usize,
+    stroke_weight: f32,
+}
+fn model(app: &App) -> Model {
+    let window_id = app
+        .new_window()
+        .view(view)
+        .raw_event(raw_window_event)
+        .build()
+        .unwrap();
+    let window = app.window(window_id).unwrap();
+    let egui = Egui::from_window(&window);
     Model {
         lines: Vec::new(),
-        angle: 0.0,
+        egui,
         speed: 1.05,
-        line_length: 15.0,
+        settings: Settings {
+            angle: 0.0,
+            line_length: 15.0,
+            r: 10.0,
+            line_count: 1000,
+            stroke_weight: 5.0,
+        },
     }
 }
-
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.angle += model.speed;
-    let r = model.angle.sqrt() * 10.0;
-    let x = r * model.angle.cos();
-    let y = r * model.angle.sin();
+    let egui = &mut model.egui;
+    let _settings = &model.settings;
+    egui.set_elapsed_time(_update.since_start);
+    let ctx = egui.begin_frame();
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        ui.label("angle:");
+        ui.add(egui::Slider::new(&mut model.settings.angle, 0.0..=1000.0));
+        ui.label("r:");
+        ui.add(egui::Slider::new(&mut model.settings.r, 0.0..=100.0));
+        ui.label("line_length:");
+        ui.add(egui::Slider::new(&mut model.settings.line_length, 0.0..=100.0));
+        ui.label("r_threshold:");
+        ui.add(egui::Slider::new(&mut model.settings.line_count, 0..=5000));
+        ui.label("stroke_weight:");
+        ui.add(egui::Slider::new(&mut model.settings.stroke_weight, 0.0..=300.0));
+        if ui.button("Restart").clicked() {
+            model.lines.clear();
+            model.settings.angle = 0.0;
+        }
+    });
+    model.settings.angle += model.speed;
+    let r = model.settings.angle.sqrt() * model.settings.r;
+    let x = r * model.settings.angle.cos();
+    let y = r * model.settings.angle.sin();
     let start = pt2(x, y);
-    let end = pt2(x + model.line_length, y + model.line_length);
+    let end = pt2(x + model.settings.line_length, y +model.settings.line_length);
     model.lines.push((start, end));
-    if model.lines.len() > 1000 {
+    if model.lines.len() > model.settings.line_count {
         model.lines.remove(0);
     }
 }
-
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
@@ -47,11 +82,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
             .start(start)
             .end(end)
             .color(color)
-            .stroke_weight(5.0);
+            .stroke_weight(model.settings.stroke_weight);
     }
-
     draw.to_frame(app, &frame).unwrap();
-    if app.keys.down.contains(&Key::Space) {
+    model.egui.draw_to_frame(&frame).unwrap();
+if app.keys.down.contains(&Key::Space) {
         let file_path = app
             .project_path()
             .expect("failed to locate project directory")
@@ -59,4 +94,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
             .join(format!("{:0}.png", app.elapsed_frames()));
         app.main_window().capture_frame(file_path);
     } 
+}
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    model.egui.handle_raw_event(event);
 }
