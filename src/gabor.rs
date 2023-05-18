50a448a04adce71b64128d035cfd205a38e26cd8
altunenes <enesaltun2@gmail.com>
2023-05-18 15:06:42 +0300

    GABOR

--- src/gabor.rs
+++ src/gabor.rs
@@ -1,17 +1,77 @@
 use nannou::prelude::*;
+use nannou_egui::{self, egui, Egui};
+
 struct Model {
     time: f32,
+    settings: Settings,
+    egui: Egui,
 }
+
+struct Settings {
+    sigma: f32,
+    t: f32,
+    u: f32,
+    v: f32,
+    kx: f32,
+    ky: f32,
+    c: usize,
+
+
+}
+
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    model.egui.handle_raw_event(event);}
+
+
 fn main() {
     nannou::app(model)
         .update(update)
-        .simple_window(view)
         .run();
 }
-fn model(_app: &App) -> Model {
-    Model { time: 0.0 }
+fn model(app: &App) -> Model {
+    let window_id = app
+        .new_window()
+        .view(view)
+        .raw_event(raw_window_event)
+        .build()
+        .unwrap();
+    let window = app.window(window_id).unwrap();
+    let egui = Egui::from_window(&window);
+    let settings = Settings {
+        sigma: 128.0,
+        t:30.0,
+        u: 128.0,
+        v: 128.0,
+        kx: 12.0,
+        ky: 5.0,
+        c: 1,
+    
+    };
+
+    Model { time: 0.0, settings, egui }
 }
 fn update(app: &App, model: &mut Model, _update: Update) {
+    let egui = &mut model.egui;
+    let settings = &mut model.settings;
+
+    egui.set_elapsed_time(_update.since_start);
+    let ctx = egui.begin_frame();
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        ui.add(egui::Slider::new(&mut settings.sigma, 0.0..=256.0).text("sigma"));
+        ui.add(egui::Slider::new(&mut settings.t, 0.0..=360.0).text("theta"));
+        ui.add(egui::Slider::new(&mut settings.u, 50.0..=256.0).text("u"));
+        ui.add(egui::Slider::new(&mut settings.v, 50.0..=256.0).text("v"));
+        ui.add(egui::Slider::new(&mut settings.kx, 0.0..=256.0).text("kx"));
+        ui.add(egui::Slider::new(&mut settings.ky, 0.0..=256.0).text("ky"));
+        ui.label(format!("color {}", settings.c));
+        if ui.button("Next color mode").clicked() {
+            settings.c = (settings.c % 2) + 1;
+        }
+
+    });    
+
+
+
     model.time = app.time;
 }
 fn gabor(x: f32, y: f32, kx_ratio: f32, ky_ratio: f32, theta: f32, sigma: f32, width: f32, height: f32) -> f32 {
@@ -22,16 +82,17 @@ fn gabor(x: f32, y: f32, kx_ratio: f32, ky_ratio: f32, theta: f32, sigma: f32, w
     (-0.5 * (x_theta.powi(2) / sigma.powi(2) + y_theta.powi(2) / sigma.powi(2))).exp()
         * (2.0 * std::f32::consts::PI * (kx * x_theta + ky * y_theta)).cos()
 }
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
+    let settings = &model.settings;
     let draw = app.draw();
     let win = app.window_rect();
     draw.background().color(WHITE);
-    let kx_ratio = 12.0;
-    let ky_ratio = 5.0;
-    let theta = (30.0_f32 + _model.time).to_radians();
-    let sigma = 128.0;
-    let width = 128.0;
-    let height = 128.0;
+    let kx_ratio = settings.kx;
+    let ky_ratio = settings.ky;
+    let theta = (settings.t + model.time).to_radians();
+    let sigma = settings.sigma;
+    let width = settings.u;
+    let height = settings.v;
     let step_x = win.w() / width;
     let step_y = win.h() / height;
     for i in 0..width as i32 {
@@ -39,17 +100,15 @@ fn view(app: &App, _model: &Model, frame: Frame) {
             let x = step_x * i as f32 - win.w() / 2.0;
             let y = step_y * j as f32 - win.h() / 2.0;
             let value = gabor(x, y, kx_ratio, ky_ratio, theta, sigma, width, height);
-            let color = nannou::color::rgb(value.abs(), value.abs(), value.abs());
+            let color = match settings.c {
+                1 => nannou::color::rgb(value.abs(), value.abs(), value.abs()),
+                2 => nannou::color::rgb((x + win.w() / 2.0) / win.w(), (y + win.h() / 2.0) / win.h(), value.abs()), 
+                _ => nannou::color::rgb(0.0, 0.0, 0.0),
+            };            
             draw.rect().x_y(x, y).w_h(step_x, step_y).color(color);
         }
     }
+
     draw.to_frame(app, &frame).unwrap();
-    if app.keys.down.contains(&Key::Space) {
-        let file_path = app
-          .project_path()
-          .expect("failed to locate project directory")
-          .join("frames") 
-          .join(format!("{:0}.png", app.elapsed_frames()));
-        app.main_window().capture_frame(file_path); 
-    } 
+    model.egui.draw_to_frame(&frame).unwrap();
 }
