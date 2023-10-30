bcc9da964861907c245b6c46132d060c120976af
altunenes <enesaltun2@gmail.com>
2023-10-30 14:15:57 +0300

    Ulam_spiral

--- src/ulam.rs
+++ src/ulam.rs
@@ -1,8 +1,12 @@
 use nannou::prelude::*;
+use nannou_egui::{self, egui, Egui};
+
 fn main() {
     nannou::app(model).update(update).run();
 }
 struct Model {
+    egui: Egui,
+    settings:Settings,
     path: Vec<(Point2, usize)>,
     counter: usize,
     dx: isize,
@@ -12,20 +16,35 @@ struct Model {
     y: isize,
     numbers: Vec<bool>,
 }
+struct Settings {
+    restart:bool,
+    stop:bool,
+
+}
+
 fn model(app: &App) -> Model {
     let _window_id = app
         .new_window()
         .size(800, 800)
         .view(view)
+        .raw_event(raw_window_event)
         .build()
         .unwrap();
-    let size = 10000;
+    let size: usize = 10000;
+    let window = app.window(_window_id).unwrap();
+
+    let egui: Egui = Egui::from_window(&window);
+
     let mut numbers = vec![false; size];
     for i in 2..size {
         if is_prime(i) {
             numbers[i] = true;
         }
     }
+    let settings = Settings {
+        restart:false,
+        stop:false,
+    };
     Model {
         path: Vec::new(),
         counter: 1,
@@ -35,9 +54,43 @@ fn model(app: &App) -> Model {
         x: 0,
         y: 0,
         numbers,
+        egui,
+        settings,
     }
 }
 fn update(_app: &App, model: &mut Model, _update: Update) {
+    let egui = &mut model.egui;
+    let settings = &mut model.settings;
+    
+    egui.set_elapsed_time(_update.since_start);
+    let ctx = egui.begin_frame();
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        if ui.button("Restart").clicked() {
+            settings.restart = true;
+        }
+        if ui.button("Stop/Resume").clicked() {
+            settings.stop = !settings.stop;
+        }
+    });
+    
+    // Your existing logic here
+    if settings.stop {
+        return;
+    }
+
+    if settings.restart {
+        model.counter = 1;
+        model.n = 1;
+        model.dx = 7;
+        model.dy = 0;
+        model.x = 0;
+        model.y = 0;
+        model.path.clear();
+        settings.restart = false;
+    }
+
+
+
     let i = model.counter;
     let x = model.x;
     let y = model.y;
@@ -91,6 +144,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
         .xy(vec2(-350.0, 350.0))
         .font_size(15);
     draw.to_frame(app, &frame).unwrap();
+    model.egui.draw_to_frame(&frame).unwrap();
+
     if app.keys.down.contains(&Key::Space) {
         let file_path = app
             .project_path()
@@ -110,4 +165,8 @@ fn is_prime(n: usize) -> bool {
         }
     }
     true
+}
+
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    model.egui.handle_raw_event(event);
 }
