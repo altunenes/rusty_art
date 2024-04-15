495ab895c583ce3d15eeca818aa1064bb98b75a8
altunenes <enesaltun2@gmail.com>
2024-04-16 01:57:47 +0300

    imfft

--- src/imgblob.rs
+++ src/imgblob.rs
@@ -1,24 +1,20 @@
 use nannou::image::{open, DynamicImage, GenericImageView, Rgba};
 use nannou::prelude::*;
 use nannou::wgpu::Texture;
-use std::path::PathBuf;
 use nannou_egui::{self, egui, Egui};
-
-fn get_image_path(relative_path: &str) -> PathBuf {
-    let current_dir = std::env::current_dir().unwrap();
-    current_dir.join(relative_path)}
+use rfd::FileDialog;
 fn main() {
     nannou::app(model).update(update).run();
 }
-
 struct Model {
-    img: DynamicImage,
+    img: Option<DynamicImage>,
     egui: Egui,
     zoom: f32,
     texture: Option<Texture>,
     scale: f32,
     settings: Settings,
 }
+
 #[allow(dead_code)]
 struct Settings {
     use_real_colors: bool,
@@ -26,15 +22,12 @@ struct Settings {
     sampling: usize,
     min_radius: f32,
     max_radius: f32,
+    open_file_dialog: bool,
+    show_ui: bool,
 }
-
 fn model(app: &App) -> Model {
-    let img_path = get_image_path("images/mona.jpg");
-    let img = open(img_path).unwrap().to_rgba8();
-    let scale = 1.0;
     let _w_id = app
         .new_window()
-        .size((img.width() as f32 * scale) as u32, (img.height() as f32 * scale) as u32)
         .view(view)
         .raw_event(raw_window_event)
         .build()
@@ -47,11 +40,13 @@ fn model(app: &App) -> Model {
         sampling: 8,
         min_radius: 0.05,
         max_radius: 2.0,
+        open_file_dialog: false,
+        show_ui: true,
     };
     Model {
-        img: DynamicImage::ImageRgba8(img),
+        img: None,
         texture: None,
-        scale,
+        scale: 1.0,
         egui,
         settings,
         zoom: 1.0,
@@ -59,88 +54,90 @@ fn model(app: &App) -> Model {
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
+    if app.keys.down.contains(&Key::H) {
+        model.settings.show_ui = !model.settings.show_ui;
+    }
     let egui = &mut model.egui;
-    let settings = &mut model.settings;
 
-    egui.set_elapsed_time(_update.since_start);
     let ctx = egui.begin_frame();
     egui::Window::new("Settings").show(&ctx, |ui| {
-        ui.label(format!("color {}", settings.colors));
-        if ui.button("next").clicked() {
-            settings.colors = (settings.colors % 3) + 1;
+        if ui.button("Load Image").clicked() {
+            model.settings.open_file_dialog = true;
         }
-
-        ui.add(egui::Slider::new(&mut settings.sampling, 4..=10).text("sampling"));
-
-        ui.add(egui::Slider::new(&mut settings.min_radius, 0.01f32..=10.0f32).text("min radius"));
-        ui.add(egui::Slider::new(&mut settings.max_radius, 0.01f32..=10.0f32).text("max radius"));
-
-        ui.add(egui::Checkbox::new(&mut settings.use_real_colors, "Use Real Colors"));
+        ui.label(format!("color {}", model.settings.colors));
+        if ui.button("next").clicked() {
+            model.settings.colors = (model.settings.colors % 3) + 1;
+        }
+        ui.add(egui::Slider::new(&mut model.settings.sampling, 4..=10).text("sampling"));
+        ui.add(egui::Slider::new(&mut model.settings.min_radius, 0.01f32..=10.0f32).text("min radius"));
+        ui.add(egui::Slider::new(&mut model.settings.max_radius, 0.01f32..=10.0f32).text("max radius"));
+        ui.add(egui::Checkbox::new(&mut model.settings.use_real_colors, "Use Real Colors"));
     });
-
-    model.texture = Some(Texture::from_image(app, &model.img));
-}
-
-fn view(app: &App, model: &Model, frame: Frame) {
-    frame.clear(WHITE);
-    let draw = app.draw().scale(model.zoom);
-
-    let (img_width, img_height) = model.img.dimensions();
-    let step_size = model.settings.sampling;
-    let time = app.time;
-
-    for y in (0..img_height).step_by(step_size) {
-        for x in (0..img_width).step_by(step_size) {
-            let pixel = model.img.get_pixel(x, y);
-            let luminance = calculate_luminance(&pixel);
-            let oscillation = (time.sin() * 0.5 + 0.5).abs();
-            let radius_state1 = map_range(luminance, 0.0, 1.0, model.settings.min_radius, model.settings.max_radius);
-            let radius_state2 = map_range(luminance, 0.0, 1.0, model.settings.max_radius, model.settings.min_radius);
-            let radius = radius_state1 * oscillation + radius_state2 * (1.0 - oscillation);
-            let x: f32 = (x as f32 - img_width as f32 / 2.0) * model.scale;
-            let y = ((img_height - y) as f32 - img_height as f32 / 2.0) * model.scale;
-            let color = if model.settings.use_real_colors {
-                let r = pixel.0[0] as f32 / 255.0;
-                let g = pixel.0[1] as f32 / 255.0;
-                let b = pixel.0[2] as f32 / 255.0;
-                rgba(r, g, b, 1.0)
-            } else{
-            match model.settings.colors {
-                1 =>
-            {
-                let progress = y / img_height as f32; 
-                let hue = progress;
-                let saturation = 1.0 - progress;
-                let lightness =  0.4 * (0.5 + time + progress * PI).sin();
-                hsla(hue, saturation, lightness, 1.0)
+    if model.settings.open_file_dialog {
+        if let Some(file_path) = FileDialog::new().pick_file() {
+            if let Ok(img) = open(&file_path) {
+                model.img = Some(img);
+                model.texture = Some(Texture::from_image(app, model.img.as_ref().unwrap()));
             }
-
-            2 => {
-                hsla(0.0, 0.0, 0.0, 1.0)
-            }
-
-            3 => { 
-                let hue = (y / img_height as f32).fract();
-                let saturation = 1.0;
-                let lightness = 0.5;
-                hsla(hue, saturation, lightness, 1.0)
-            },
-
-            _ => unreachable!(),
-        }.into()
-    };
-
-            draw.ellipse()
-                .x_y(x, y)
-                .radius(radius)
-                .color(color);
-
+            model.settings.open_file_dialog = false;
         }
     }
+    if model.img.is_some() {
+        model.texture = model.img.as_ref().map(|img| Texture::from_image(app, img));
+    }
+}
+fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(WHITE);
+    if let Some(ref img) = model.img {
+        let draw = app.draw().scale(model.zoom);
+        let (img_width, img_height) = img.dimensions();
+        let step_size = model.settings.sampling;
+        let time = app.time;
+        for y in (0..img_height).step_by(step_size) {
+            for x in (0..img_width).step_by(step_size) {
+                let pixel = img.get_pixel(x, y);
+                let luminance = calculate_luminance(&pixel);
+                let oscillation = (time.sin() * 0.5 + 0.5).abs();
+                let radius_state1 = map_range(luminance, 0.0, 1.0, model.settings.min_radius, model.settings.max_radius);
+                let radius_state2 = map_range(luminance, 0.0, 1.0, model.settings.max_radius, model.settings.min_radius);
+                let radius = radius_state1 * oscillation + radius_state2 * (1.0 - oscillation);
+                let x: f32 = (x as f32 - img_width as f32 / 2.0) * model.scale;
+                let y = ((img_height - y) as f32 - img_height as f32 / 2.0) * model.scale;
+                let color = if model.settings.use_real_colors {
+                    rgba(pixel.0[0] as f32 / 255.0, pixel.0[1] as f32 / 255.0, pixel.0[2] as f32 / 255.0, 1.0)
+                } else {
+                    match model.settings.colors {
+                        1 => {
+                            let progress = y / img_height as f32; 
+                            let hue = progress;
+                            let saturation = 1.0 - progress;
+                            let lightness =  0.4 * (0.5 + time + progress * PI).sin();
+                            hsla(hue, saturation, lightness, 1.0)
+                        }
+                        2 => hsla(0.0, 0.0, 0.0, 1.0),
+                        3 => { 
+                            let hue = (y / img_height as f32).fract();
+                            let saturation = 1.0;
+                            let lightness = 0.5;
+                            hsla(hue, saturation, lightness, 1.0)
+                        },
+                        _ => unreachable!(),
+                    }.into()
+                };
 
-    draw.to_frame(app, &frame).unwrap();
-    model.egui.draw_to_frame(&frame).unwrap();
+                draw.ellipse()
+                    .x_y(x, y)
+                    .radius(radius)
+                    .color(color);
+            }
+        }
 
+        draw.to_frame(app, &frame).unwrap();
+    }
+
+    if model.settings.show_ui {
+        model.egui.draw_to_frame(&frame).unwrap();
+    }
     if app.keys.down.contains(&Key::Space) {
         let file_path = app
             .project_path()
@@ -148,16 +145,14 @@ fn view(app: &App, model: &Model, frame: Frame) {
             .join("frames")
             .join(format!("{:0}.png", app.elapsed_frames()));
         app.main_window().capture_frame(file_path);
-    } 
+    }
 }
-
 fn calculate_luminance(pixel: &Rgba<u8>) -> f32 {
     let r = pixel[0] as f32 / 255.0;
     let g = pixel[1] as f32 / 255.0;
     let b = pixel[2] as f32 / 255.0;
     0.2126 * r + 0.7152 * g + 0.0722 * b
 }
-
 fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
     model.egui.handle_raw_event(event);
     if let nannou::winit::event::WindowEvent::MouseWheel { delta, .. } = event {
@@ -170,6 +165,15 @@ fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event:
                 }
                 _ => (),
             }
+        }
+    }
+    if let nannou::winit::event::WindowEvent::KeyboardInput { input, .. } = event {
+        if let (Some(nannou::winit::event::VirtualKeyCode::F), true) =
+            (input.virtual_keycode, input.state == nannou::winit::event::ElementState::Pressed)
+        {
+            let window = _app.main_window();
+            let fullscreen = window.fullscreen().is_some();
+            window.set_fullscreen(!fullscreen);
         }
     }
 }
